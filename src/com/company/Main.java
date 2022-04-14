685bb7d37027f61a5d92eb3b6e0d83d4c5d0c368
John Poul Barangan <baranganjanjan30@gmail.com>
2022-04-14 14:45:11 +0800

    initial-commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("John Poul R. Barangan");
+        System.out.println("BSIT 1A");
+
+    }
+}
